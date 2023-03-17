4b2c2c5906b4cedbec4020d65b65595fded1d90b
maryam <m.shakerimaryam@gmail.com>
2023-03-17 16:12:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Pick up the Umbrella Reminder
+
+## Description
+In this code, the weather condition of the current day is checked and if the weather is not clear, an SMS is sent to
+the user to bring an umbrella.
+
+For this purpose, the user must first register at https://openweathermap.org/ and https://console.twilio.com/.
+(no payment is required to use this cap)
+
+The first site will provide the user with an API to receive today's weather information. (More information in 
+the API document: https://openweathermap.org/api)
+
+The second site provides a virtual number to the user so that he can send SMS or WhatsApp notifications to
+others (approved numbers).
+(More information in the document https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1)
+
+After registration, the user must set the specific parameters:
+- Latitude & longitude (You can search on the internet: For example the site: https://www.maps.ie/coordinates.html)
+- api_key received from openweathermap;
+- account_sid, auth_token and virtual number received from twilio.
+- The number of the recipient of the SMS, which was previously verified on twilio.
+
+## How to run
+run following:
+```bash
+python -m venv env
+. env/bin/activate
+pip install -r requirements.txt
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,76 @@
+"""---------------------------------------- Pick up the Umbrella Reminder ----------------------------------------
+
+In this code, the weather condition of the current day is checked and if the weather is not clear, an SMS is sent to
+the user to bring an umbrella.
+
+For this purpose, the user must first register at https://openweathermap.org/ and https://console.twilio.com/.
+(no payment is required to use this cap)
+
+The first site will provide the user with an API to receive today's weather information. (More information in the API
+technical document: https://openweathermap.org/api)
+
+The second site provides a virtual number to the user so that he can send SMS or WhatsApp notifications to
+others (approved numbers).
+(More information in the technical document https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1)
+
+After registration, the user must set the specific parameters:
+- Latitude & longitude (You can search on the internet: For example the site: https://www.maps.ie/coordinates.html)
+- api_key received from openweathermap;
+- account_sid, auth_token and virtual number received from twilio.
+- The number of the recipient of the SMS, which was previously verified on twilio.
+
+"""
+
+# ---------------------------------------- Add Required Library ----------------------------------------
+
+import os
+import requests
+from twilio.rest import Client
+
+# ---------------------------------------- Specific Parameters Definition ----------------------------------------
+
+my_lat = "MY_LAT"
+my_long = "MY_LONG"
+api_key = "API_KEY"
+account_sid = "ACCOUNT_SID"
+auth_token = "AUTH_TOKEN"
+sender = "SENDER"
+receiver = "RECEIVER"
+
+# ---------------------------------------- Weather API call ----------------------------------------
+
+parameters = {
+    "lat": os.getenv(my_lat),
+    "lon": os.getenv(my_long),
+    "appid": os.getenv(api_key),
+    "exclude": "current,hourly,daily"
+}
+
+weather_data = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters)
+
+# ---------------------------------------- Checking for Errors in api calls ----------------------------------------
+
+weather_data.raise_for_status()
+
+# ---------------------------------------- Data to Json Conversion ----------------------------------------
+
+data = weather_data.json()
+
+# ---------------------------------------- Required data Selection ----------------------------------------
+
+weather_condition = data["weather"][0]["id"]
+
+# ---------------------------------------- Need to Inform Assessment ----------------------------------------
+
+need_umbrella = False
+
+if weather_condition > 700:
+    need_umbrella = True
+    client = Client(os.getenv(account_sid), os.getenv(auth_token))
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an ☂️.",
+        from_=os.getenv(sender),
+        to=os.getenv(receiver)
+    )
+
+    print(message.status)
